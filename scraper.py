2f1f9dd0574a8aeadb663b31fd417e14dafaf39c
Paola Duran <duranpao0618@gmail.com>
2021-03-07 17:29:40 -0400

    Saving the news in text files

--- scraper.py
+++ scraper.py
@@ -1,12 +1,44 @@
 import requests
 import lxml.html as html
+import os
+import datetime
 
 HOME_URL = 'https://www.larepublica.co/'
 
 XPATH_LINK_TO_ARTICLE = '//div[@class="news V_Title_Img"]/a/@href'
-XPATH_TITLE = '//div[@class="mb-auto"]/h2/span/text()'
+XPATH_TITLE = '//div[@class="mb-auto"]/text-fill/span/text()'
 XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
 XPATH_BODY = '//div[@class="html-content"]/p[not(@class)]/text()'
+
+
+def parse_notice(link, today):
+    try:
+        response = requests.get(link)
+        if response.status_code == 200:
+            notice = response.content.decode('utf-8')
+            parsed = html.fromstring(notice)
+
+            try:
+                title = parsed.xpath(XPATH_TITLE)
+                title[0] = title[0].replace('\"', '')
+                summary = parsed.xpath(XPATH_SUMMARY)
+                body = parsed.xpath(XPATH_BODY)
+            except IndexError:
+                return
+
+            with open('{}/{}.txt'.format(today, title), 'w', encoding='utf-8') as f:
+                f.write(title[0])
+                f.write('\n\n')
+                f.write(summary[0])
+                f.write('\n\n')
+                for p in body:
+                    f.write(p)
+                    f.write('\n')
+        else:
+            raise ValueError('Error: {}'.format(response.status_code))
+    except ValueError as ve:
+        print(ve)
+
 
 def parse_home():
     try:
@@ -15,9 +47,15 @@ def parse_home():
             home = response.content.decode('utf-8')
             parser = html.fromstring(home)
             links_to_notices = parser.xpath(XPATH_LINK_TO_ARTICLE)
-            print(links_to_notices)
+
+            today = datetime.date.today().strftime('%d-%m-%y')
+            if not os.path.isdir(today):
+                os.mkdir(today)
+
+            for link in links_to_notices:
+                parse_notice(link, today)
         else:
-            raise ValueError('Error: {}'.format(esponse.status_code))
+            raise ValueError('Error: {}'.format(response.status_code))
     except ValueError as ve:
         pass
 
